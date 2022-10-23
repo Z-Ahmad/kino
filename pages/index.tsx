0e1fb9e8116feb8ef722ffbf55bb35e73cbdfe42
Z-Ahmad <zakiahmad42661@gmail.com>
2022-10-22 23:54:22 -0400

    replaced favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <div className='relative h-screen bg-zinc-900'>
       <Head>
         <title>Home - Kino</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <div className='text-xl font-large text-white'>Welcome to Kino</div>
     </div>
